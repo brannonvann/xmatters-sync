484ac338b422c84c7938438a0c0a2683aeeca1f5
Hayden Miedema <haydenmiedema@gmail.com>
2020-10-02 12:23:47 -0400

    update config and setup scripts

--- config.js
+++ config.js
@@ -1,10 +1,31 @@
 #!/usr/bin/env node
 const xm = require("xmtoolbox");
 const env = require('dotenv').config();
+const fs = require('fs')
+const { exec } = require("child_process");
+// var execSync = require('exec-sync');
 
 //Set credentials environment variables or in this file by replaceing the examples below.
 //Defaults to using environment variables if they are set.
 //Create a .env file in the same directory as the project and set the environment variables there for them to be used below
+//Check if .env file is created before
+if(!fs.existsSync("./.env")){
+  execSync('npm run setup');
+//   exec("npm run setup", (error, stdout, stderr) => {
+//     if (error) {
+//         console.log(`error: ${error.message}`);
+//         return;
+//     }
+//     if (stderr) {
+//         console.log(`stderr: ${stderr}`);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout}`);
+// });
+  // console.log("ERROR: .env file does not exist. Please run the setup scripts first " +
+  //  "by running 'npm run setup' or 'npm run setupAndSync' to setup and sync data.")
+  //  process.exit(0);
+}
 const PROD_SUBDOMAIN = process.env.PROD_SUBDOMAIN || "company"; //company from url--> https://company.xmatters.com
 const PROD_USERNAME = process.env.PROD_USERNAME || "user";
 const PROD_PASSWORD = process.env.PROD_PASSWORD || "password";
--- setup.js
+++ setup.js
@@ -7,7 +7,13 @@ var username = readlineSync.question("Enter PROD_USERNAME: ");
 var password = readlineSync.question("Enter PROD_PASSWORD: ");
 var licenseLimit = readlineSync.question("Enter license limit # of users: ");
 var minUsers = readlineSync.question("Enter minimum number of users in input file to run: ");
+var shouldBackup = readlineSync.question("Run a backup before syncing? (Y or N): ");
+if(shouldBackup.toLowerCase() === 'y'){
+    shouldBackup = 'true';
+}else{
+    shouldBackup = 'false';
+}
 var config = "PROD_SUBDOMAIN=" + subdomain + "\nPROD_USERNAME=" + username + "\nPROD_PASSWORD=" + password +
-                "\nLICENSELIMIT=" + licenseLimit + "\nMIN_USERS=" + minUsers;
+                "\nLICENSELIMIT=" + licenseLimit + "\nMIN_USERS=" + minUsers + "\nSHOULD_BACKUP=" + shouldBackup;
 fs.writeFileSync("./.env", config);
 console.log("Environment settings written to .env file. Old contents overwritten.");
